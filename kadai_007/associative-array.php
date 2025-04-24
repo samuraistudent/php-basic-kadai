1e01fef4fcaa91e19d6fe7c50db5c70309641d93
samuraistudent <trickers0826@gmail.com>
2025-04-24 14:19:35 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>
+    <?php
+    $vegetable_deta = ["name" => "onion", "price" => 200, "weight" => 160];
+    print_r($vegetable_deta);
+    ?>
+   </p>
+</body>
+
+</html>
